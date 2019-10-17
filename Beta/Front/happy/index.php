42848ab0828ed69aa8fd5c57f8bc17aac83d5b1d
ntb6 <56608716+ntb6@users.noreply.github.com>
2019-10-17 18:33:38 -0400

    Create index.php

---
+++ Beta/Front/happy/index.php
@@ -0,0 +1,17 @@
+<?php
+	$input = file_get_contents("php://input");
+
+  $ch = curl_init();
+	//curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~ndl25/backupProject/beta/back/login.php");
+  //curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~ts557/login.php");
+  curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~gvc3/mLogin.php");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+	$result = curl_exec($ch);
+  echo $result;
+
+	curl_close($ch);
+?>
