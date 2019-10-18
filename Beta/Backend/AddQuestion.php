28e384cf391106a27037b20f1d75e83f009b0c33
karanpatel3 <39688706+karanpatel3@users.noreply.github.com>
2019-10-18 15:26:31 -0400

    Create AddQuestion.php

---
+++ Beta/Backend/AddQuestion.php
@@ -0,0 +1,23 @@
+<?php
+
+include('connectSQL.php');
+
+$Question= $_POST['problem'];
+$Topic= $_POST['topic'];
+$Difficulty=$_POST['difficulty'];
+$TestCase1=$_POST['test_case_1'];
+$TestCase2=$_POST['test_case_2'];
+$query = "INSERT INTO `kp486`.`Question_Bank` (`Question`, `Topic`, `Difficulty`, `TestCase1`, `TestCase2`)VALUES ('$Question', '$Topic', '$Difficulty', '$TestCase1', '$TestCase2')";
+
+$fail = fail;
+
+$success = success;
+
+if ($conn->query($query) === TRUE) {
+    echo json_encode($success);
+} else {
+    echo json_encode("Error: " . $query . "<br>" . $conn->error);
+}
+  $conn->close();
+ 
+?>
