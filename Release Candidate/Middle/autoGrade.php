edbaa02e096f9841ac69a8ca33bb9800da4280da
Leonidous <43423871+Leonidous@users.noreply.github.com>
2019-11-13 21:05:36 -0500

    Update autoGrade.php

--- Release Candidate/Middle/autoGrade.php
+++ Release Candidate/Middle/autoGrade.php
@@ -10,16 +10,19 @@ print(sum((8, 2, 3, 0, 7)))";
 $points = 10;
 
 $functionName = "sum";
-$constraint = "while";
+$constraint = "for";
+
+$testcase1 = "0";
+$testcase2 = "0";
+$testcase3 = "0";
+$testcase4 = "0";
+$testcase5 = "0";
+$testcase6 = "0";
 
 function checkColon($ans){
     $answer = $ans;
-    $answerFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/answer.txt", "w+") or die("Unable to open file.");
-    fwrite($answerFile, $answer);
-    rewind($answerFile);
-    $line = fgets($answerFile);
 
-    fclose($answerFile);
+    $line = strstr($answer,"\n",true);
 
     if (strstr($line, ':')) {
         return true;
@@ -33,17 +36,28 @@ function checkFuncName($funcName, $ans){
     $funcFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/funcName.txt", "w") or die("Unable to open file.");
 
     $answer = $ans;
-    $answerFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/answer.txt", "w") or die("Unable to open file.");
 
     fwrite($funcFile, $functionName);
-    fwrite($answerFile, $answer);
+
+    if(!checkColon($answer)){
+        $answerFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/answer.txt", "w") or die("Unable to open file.");
+        $firstline = strstr($answer,"\n",true);
+        $firstline = rtrim($firstline);
+        $colon =':';
+        $newAnswer = str_replace($firstline, $firstline.$colon, $answer);
+        fwrite($answerFile, $newAnswer);
+    }else{
+        $answerFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/answer.txt", "w") or die("Unable to open file.");
+        fwrite($answerFile, $answer);
+    }
 
     $execResult = shell_exec('python /afs/cad.njit.edu/u/a/j/ajr74/public_html/funCheck.py');
 
     fclose($answerFile);
     fclose($funcFile);
 
-    if($execResult = "Correct function name"){
+    $execResult = trim($execResult);
+    if($execResult == "Correct function name"){
         return true;
     } else {
         return false;
@@ -61,40 +75,59 @@ function checkConstraint($cons, $ans){
     }
 }
 
-function output($inPoints, $funcName, $cons, $ans){
+function output($inPoints, $funcName, $cons, $ans, $ind){
     $points = $inPoints;
     $answer = $ans;
     $functionName = $funcName;
     $constraint = $cons;
     $wrongCounter = 0;
-
-    if(checkColon($answer)){
+    $indicator = $ind;
+    if(!checkColon($answer)){
         $minus = ceil($points*0.10);
         $colonMsg = "No : in header, minus " . $minus . " points";
-        wrongCounter++;
+        $wrongCounter++;
     }
-    if(checkConstraint($constraint, $answer)){
-        $minus = ceil($points*0.10);
-        $constraintMsg = "No" . $constraint . " loop used in answer, minus " . $minus . " points";
-        wrongCounter++;
-    }
-    if(checkFuncName($functionName, $answer)){
+    if(!checkFuncName($functionName, $answer)){
         $minus = ceil($points*0.10);
         $funcNameMsg = "Incorrect function name, minus " . $minus . " points";
-        wrongCounter++;
+        $wrongCounter++;
+    }
+    if(!checkConstraint($constraint, $answer)){
+        $minus = ceil($points*0.10);
+        $constraintMsg = "No " . $constraint . " statement used in answer, minus " . $minus . " points";
+        $wrongCounter++;
+    }
+
+    if($wrongCounter == 0){
+        return "Nothing Wrong";
+    }else{
+        if($indicator == 1){
+            return $colonMsg;
+        }elseif ($indicator == 2){
+            return $funcNameMsg;
+        }elseif ($indicator == 3){
+            return $constraintMsg;
+        }else{
+            return "No indicator";
+        }
     }
 }
 
+function checkTestCases($answer, $testcase1, $testcase2, $testcase3, $testcase4, $testcase5, $testcase6){
+
+}
+
+$i = 1;
 
 $output = checkColon($answer);
 $output2 = checkFuncName($functionName, $answer);
 $output3 = checkConstraint($constraint, $answer);
-
-print($output);
+$output4 = output($points, $functionName, $constraint, $answer, $i);
+print($output . "Colon: ");
 print("<br>");
-print($output2);
+print($output2 . "Function Name");
 print("<br>");
-print($output3);
+print($output3 . "Constraint");
 print("<br>");
-
+print($output4);
 ?>
