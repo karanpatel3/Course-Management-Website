d3c95807deab2637b573d6960a1a02e7814605fb
Leonidous <43423871+Leonidous@users.noreply.github.com>
2019-11-15 18:04:11 -0500

    Update autoGrade.php

--- Release Candidate/Middle/autoGrade.php
+++ Release Candidate/Middle/autoGrade.php
@@ -1,18 +1,19 @@
 <?php
-
-$answer = "def sum(numbers):
-    total = 0
-    for x in numbers:
-        total += x
-    return total
-print(sum((8, 2, 3, 0, 7)))";
-
+$answer = "def operation(op, a, b):
+	if op == '+':
+		return a+b
+	elif op == '-':
+		return a-b
+	elif op == '*':
+		return a*b
+	elif op == '/':
+		return a/b
+	else:
+		return \"error\"";
 $points = 10;
-
-$functionName = "sum";
-$constraint = "for";
-
-$testcase1 = "0";
+$functionName = "operation";
+$constraint = "";
+$testcase1 = "\"-\",5,3/2";
 $testcase2 = "0";
 $testcase3 = "0";
 $testcase4 = "0";
@@ -21,22 +22,17 @@ $testcase6 = "0";
 
 function checkColon($ans){
     $answer = $ans;
-
     $line = strstr($answer,"\n",true);
-
     if (strstr($line, ':')) {
         return true;
     } else {
         return false;
     }
 }
-
 function checkFuncName($funcName, $ans){
     $functionName = $funcName;
     $funcFile = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/funcName.txt", "w") or die("Unable to open file.");
-
     $answer = $ans;
-
     fwrite($funcFile, $functionName);
 
     if(!checkColon($answer)){
@@ -63,15 +59,59 @@ function checkFuncName($funcName, $ans){
         return false;
     }
 }
-
 function checkConstraint($cons, $ans){
     $answer = $ans;
     $constraint = $cons;
-
     if (strstr($answer, $constraint)) {
         return true;
     } else {
         return false;
+    }
+}
+
+function checkTestcase($test, $ans){
+
+    $testcase = $test;
+    $answer = $ans;
+
+    $funcName = shell_exec('python /afs/cad.njit.edu/u/a/j/ajr74/public_html/funcName.py');
+
+    $funcName = trim($funcName);
+
+    $split = explode("/", $testcase);
+
+    $input = explode(",", $split[0]);
+    $output = $split[1];
+
+    $paraNum = sizeof($input);
+    $runline = "";
+
+    for($x = 0; $x < $paraNum; $x++){
+        if($x == 0){
+            $runline = $funcName . "(" . $input[$x];
+        }else if($x != $paraNum && $x != 0){
+            $runline = $runline . ',' . $input[$x];
+        }
+
+        if($x == $paraNum-1){
+            $runline = $runline . ")";
+        }
+    }
+
+    $answer = $answer . "\n" . "print(" . $runline . ")";
+
+    $file = fopen("/afs/cad.njit.edu/u/a/j/ajr74/public_html/pythonExec.py", "w") or die("Unable to open file.");
+
+    fwrite($file, $answer);
+
+    $pyOut = shell_exec('python /afs/cad.njit.edu/u/a/j/ajr74/public_html/pythonExec.py');
+
+    $pyOut = trim($pyOut);
+
+    if($pyOut != $output){
+        return false;
+    }else{
+        return true;
     }
 }
 
@@ -97,7 +137,6 @@ function output($inPoints, $funcName, $cons, $ans, $ind){
         $constraintMsg = "No " . $constraint . " statement used in answer, minus " . $minus . " points";
         $wrongCounter++;
     }
-
     if($wrongCounter == 0){
         return "Nothing Wrong";
     }else{
@@ -113,21 +152,10 @@ function output($inPoints, $funcName, $cons, $ans, $ind){
     }
 }
 
-function checkTestCases($answer, $testcase1, $testcase2, $testcase3, $testcase4, $testcase5, $testcase6){
-
-}
-
-$i = 1;
-
-$output = checkColon($answer);
-$output2 = checkFuncName($functionName, $answer);
-$output3 = checkConstraint($constraint, $answer);
-$output4 = output($points, $functionName, $constraint, $answer, $i);
-print($output . "Colon: ");
+$out = checkFuncName($functionName, $answer);
+$out2 = checkTestcase($testcase1, $answer);
 print("<br>");
-print($output2 . "Function Name");
+print($out2 . "Testcase");
 print("<br>");
-print($output3 . "Constraint");
 print("<br>");
-print($output4);
 ?>
