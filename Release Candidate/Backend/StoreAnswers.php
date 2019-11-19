cb2dacd0aef60d3a7f0a87053e4a1068fbbc5397
karanpatel3 <39688706+karanpatel3@users.noreply.github.com>
2019-11-18 20:12:53 -0500

    Update StoreAnswers.php

--- Release Candidate/Backend/StoreAnswers.php
+++ Release Candidate/Backend/StoreAnswers.php
@@ -13,12 +13,11 @@ foreach($json_decode as $item) { //foreach loop meant to iterate through the arr
 $result  = $conn -> query("INSERT INTO `kp486`.`TakenExam` (`Question`, `Points`, `StudentAnswer`) VALUES ('".$item['Question']."', '".$item['Points']."', '".$item['Answer']."')"); //runs query and inserts the rows into the database
      } 
      
-     echo $result;
+     //echo $result;
      
 $fail = fail; //variable to print fail
 
 $success = success; //variable to print success
-
 
 if ($result === TRUE) { //if query worked, then print success
     echo json_encode($success);
@@ -26,6 +25,45 @@ if ($result === TRUE) { //if query worked, then print success
 } else { //else print failed
     echo json_encode("Error: " . $query . "<br>" . $conn->error);
 }
+
+
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "http://afsaccess1.njit.edu/~ajr74/TESTINGONLY.php");
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$output = curl_exec($ch);
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+echo var_dump($output);
+
+$json_decode1 = json_decode($output, true); //decodes the json passed into this file into an array
+
+foreach($json_decode1 as $item) { //foreach loop meant to iterate through the array
+
+$result  = $conn -> query("INSERT INTO `kp486`.`AutoGradedExam` (`QID`, `MaxPoints`, `StudentAnswer`, `PointsAwarded`, `DeductPoints`, `AutoComments`) VALUES ('".$item[0]."', '".$item[1]."', '".$item[2]."', '".$item[3]."', '".$item[4]."', '".$item[5]."')"); //runs query and inserts the rows into the database
+     } 
+     
+     //echo $result;
+     
+$fail = fail; //variable to print fail
+
+$success = success; //variable to print success
+
+if ($result === TRUE) { //if query worked, then print success
+    echo json_encode($success);
+
+} else { //else print failed
+    echo json_encode("Error: " . $query . "<br>" . $conn->error);
+}
+
 $conn->close();
 
 ?>
